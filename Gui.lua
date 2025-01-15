e346343ee6c9023bd75299c052b24c7b2747acf0
Ww1avi <kandonmezmomo@gmail.com>
2025-01-15 15:08:25 +0100

    Gui.lua

--- Gui.lua
+++ Gui.lua
@@ -63,31 +63,3 @@ Content = <string> - The content of the notification.
 Image = <string> - The icon of the notification.
 Time = <number> - The duration of the notfication.
 ]]
-
-Tab:AddButton({
-	Name = "Infinite Jump",
-	Callback = local InfiniteJumpEnabled = true
-game:GetService("UserInputService").JumpRequest:connect(function()
-	if InfiniteJumpEnabled then
-		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
-	end
-end)
-local InfiniteJump = CreateButton("Infinite Jump: On", StuffFrame)
-InfiniteJump.Position = UDim2.new(0,10,0,130)
-InfiniteJump.Size = UDim2.new(0,150,0,30)
-InfiniteJump.MouseButton1Click:connect(function()
-	local state = InfiniteJump.Text:sub(string.len("Infinite Jump: ") + 1) --too lazy to count lol
-	local new = state == "Off" and "On" or state == "On" and "Off"
-	InfiniteJumpEnabled = new == "On"
-	InfiniteJump.Text = "Infinite Jump: " .. new
-end)
-   end,
-})()
-      		print("button pressed")
-  	end    
-})
-
---[[
-Name = <string> - The name of the button.
-Callback = <function> - The function of the button.
-]]
