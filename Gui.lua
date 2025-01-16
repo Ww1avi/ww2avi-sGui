0064b368f10d28fdcc9d70044a072347a166cb6d
Ww1avi <kandonmezmomo@gmail.com>
2025-01-16 19:58:16 +0100

    Gui.lua

--- Gui.lua
+++ Gui.lua
@@ -229,3 +229,13 @@ spawn(autoPunch)
 })
 
 local Paragraph = Tab:CreateParagraph({Title = "Enjoy!", Content = "Script Made by Ww2avi"})
+
+local Toggle = Tab:CreateToggle({
+   Name = "Toggle Example",
+   CurrentValue = false,
+   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+   -- The function that takes place when the toggle is pressed
+   -- The variable (Value) is a boolean on whether the toggle is true or false
+   end,
+})
