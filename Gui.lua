df68e856c6534858a4e43439b6c3dc8521186cab
Ww1avi <kandonmezmomo@gmail.com>
2025-01-16 17:11:50 +0100

    Gui.lua

--- Gui.lua
+++ Gui.lua
@@ -40,13 +40,7 @@ local Button = Tab:CreateButton({
    Name = "Auto hit Ancient jungle rock",
    Callback = function()
    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-7708.71, 6.81, 2893.96)
-   end,
-})
-
-local Button = Tab:CreateButton({
-   Name = "Start auto Punch",
-   Callback = function()
-   -- Auto-Punch Script for Muscle Legends
+         -- Auto-Punch Script for Muscle Legends
 local player = game.Players.LocalPlayer
 local tool = player.Backpack:FindFirstChild("Punch") -- Replace "Punch" with the tool's name
 
@@ -57,7 +51,7 @@ end
 -- Function to simulate punching
 local function autoPunch()
     while true do
-        wait(0.1) -- Delay between punches
+        wait(0.5) -- Delay between punches
         if tool and tool.Parent == player.Character then
             tool:Activate() -- Simulate punch activation
         end
